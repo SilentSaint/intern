a2682735a98e7a29e65fe0aabf64a67330f52395
vatsa-hegde <shreevatsa4321@gmail.com>
2020-07-13 23:31:35 +0530

    fade in done just add fade-in in every section's class

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+  threshold: 0.25,
+  rootMargin: "0px 0px -75px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+  entries.forEach( entry => {
+    if (!entry.isIntersecting) {
+      return;
+    }else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+
+},appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+
 Number.prototype.format = function(n) {
     var r = new RegExp('\\d(?=(\\d{3})+' + (n > 0 ? '\\.' : '$') + ')', 'g');
     return this.toFixed(Math.max(0, Math.floor(n))).replace(r, '$&,');
