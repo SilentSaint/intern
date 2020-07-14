ae2436de4c12b50c68a0ae6ff1d09127a7321ccf
vatsa-hegde <shreevatsa4321@gmail.com>
2020-07-14 20:49:38 +0530

    Our Numbers Done

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
-const header=document.querySelector("header");
-
-const faders = document.querySelectorAll(".fade-in");
-const appearOptions = {
-  threshold: 0.25,
-  rootMargin: "0px 0px -75px 0px"
-};
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
-  entries.forEach( entry => {
-    if (!entry.isIntersecting) {
-      return;
-    }else {
-      console.log(entry.target.classList);
-      entry.target.classList.add('appear');
-      appearOnScroll.unobserve(entry.target);
-    }
-  });
-
-},appearOptions);
-
-faders.forEach(fader => {
-  appearOnScroll.observe(fader);
-});
-
-
 Number.prototype.format = function(n) {
     var r = new RegExp('\\d(?=(\\d{3})+' + (n > 0 ? '\\.' : '$') + ')', 'g');
     return this.toFixed(Math.max(0, Math.floor(n))).replace(r, '$&,');
 };
 
-$('.count').each(function () {
-    $(this).prop('counter', 0).animate({
-        counter: $(this).text()
-    }, {
-        duration: 10000,
-        easing: 'easeOutExpo',
-        step: function (step) {
-            $(this).text('' + step.format());
-        }
-    });
+
+const countSection = document.querySelectorAll(".Num");
+const countOptions = {
+  threshold: 0.15,
+  rootMargin: "0px 0px -75px 0px"
+};
+const countOnScroll = new IntersectionObserver(function(entries, countOnScroll){
+  entries.forEach( entry => {
+    if (!entry.isIntersecting) {
+      return;
+    }else {
+      $('.count').each(function () {
+          $(this).prop('counter', 0).animate({
+              counter: $(this).text()
+          }, {
+              duration: 2000,
+              easing: 'easeOutExpo',
+              step: function (step) {
+                  $(this).text('' + step.format());
+              }
+          });
+      });
+    }
+  });
+
+},countOptions);
+
+countSection.forEach(fader => {
+  countOnScroll.observe(fader);
 });
